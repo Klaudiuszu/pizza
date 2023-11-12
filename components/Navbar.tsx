e807c29c6a8cd78142a99ef6ba11250d63794a6f
Klaudiusz <klaudiuszpupiec.kontakt@gmail.com>
2023-11-12 23:42:50 +0100

    add hamburger menu

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,11 @@
-import Link from "next/link"
-import Image from "next/image"
-import { NAV_LINKS } from "@/constants"
-import Button from "./Button"
+"use client";
+import Link from "next/link";
+import Image from "next/image";
+import { NAV_LINKS } from "@/constants";
+import { HamburgerMenu } from "./hamburgerMenu";
 
 const Navbar = () => {
+
   return (
     <nav className=" flexBetween max-container padding-container relative z-30 py-5">
       <Link href="/">
@@ -11,20 +13,19 @@ const Navbar = () => {
       </Link>
       <ul className="hidden h-full lg:flex">
         {NAV_LINKS.map((link) => (
-          <Link href={link.href} key={link.key} className="navbar-link regular-16 flexCenter cursor-pointer pb-1.5">
+          <Link
+            href={link.href}
+            key={link.key}
+            className="navbar-link regular-16 flexCenter cursor-pointer pb-1.5"
+          >
             {link.label}
-          </Link>  
+          </Link>
         ))}
       </ul>
-      <div className="lg:flexCenter hidden">
-        <Button 
-          type="button"
-          title="test"
-          variant="btn_dark_green"
-        />
-      </div>
+        <HamburgerMenu />
+      <div className="lg:flexCenter hidden"></div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
