251ca9e2591d9601f5c70433bfa1984d3d34af76
Klaudiusz <klaudiuszpupiec.kontakt@gmail.com>
2023-11-18 17:56:48 +0100

    add navbar hover and upgrade design

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,26 +5,29 @@ import { NAV_LINKS } from "@/constants";
 import { HamburgerMenu } from "./hamburgerMenu";
 
 const Navbar = () => {
-
   return (
-    <nav className=" flexBetween max-container padding-container relative z-30 py-5">
-      <Link href="/">
-        <Image src="/ARK PIZZA.svg" alt="PIZZA" width={74} height={29} />
-      </Link>
-      <ul className="hidden h-full lg:flex">
-        {NAV_LINKS.map((link) => (
-          <Link
-            href={link.href}
-            key={link.key}
-            className="navbar-link regular-16 flexCenter cursor-pointer pb-1.5"
-          >
-            {link.label}
-          </Link>
-        ))}
-      </ul>
+    <div className="navbar-bg">
+      <nav className="flexBetween max-container padding-container relative z-30 py-8">
+        <Link href="/">
+          <Image src="/ARK PIZZA.svg" alt="PIZZA" width={74} height={29} />
+        </Link>
+        <ul className="hidden h-full lg:flex">
+  {NAV_LINKS.map((link) => (
+    <Link
+      href={link.href}
+      key={link.key}
+      className="navbar-link regular-16 flexCenter cursor-pointer relative pb-1.5"
+    >
+      {link.label}
+      <div className="line"></div>
+    </Link>
+  ))}
+</ul>
+
         <HamburgerMenu />
-      <div className="lg:flexCenter hidden"></div>
-    </nav>
+        <div className="lg:flexCenter hidden"></div>
+      </nav>
+    </div>
   );
 };
 
